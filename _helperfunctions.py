2d50d4088b2f03e6666bd077835ddbe00e48551e
Hungerlaender Niklas-Aron <niklas-aron.hungerlaender@ait.ac.at>
2020-08-25 18:03:27 +0200

    fixed days selection

--- _helperfunctions.py
+++ _helperfunctions.py
@@ -12,7 +12,7 @@ def daterange(date1, date2):
 def next_weekday(weekday):
     d = datetime.now()
     days_ahead = weekday - d.weekday()
-    if days_ahead < 0:
+    if days_ahead <= 0:
         days_ahead += 7
     datetime_new = d + timedelta(days_ahead)
     return datetime_new.date()
