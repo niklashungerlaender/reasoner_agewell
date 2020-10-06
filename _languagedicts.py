037b0cb9075ad7dfe191eeeb745421d02ed6c1f3
Hungerlaender Niklas-Aron <niklas-aron.hungerlaender@ait.ac.at>
2020-10-06 15:13:39 +0200

    added en/de text for avatar

--- _languagedicts.py
+++ _languagedicts.py
@@ -28,7 +28,7 @@ ipaq = {
     "minutes_content": dict(en="Minutes per day:", de="Minuten pro Tag:", it="Minuti al giorno:", nl="Minuten per dag"),
     "days": dict(en="Days per week:", de="Tage pro Woche:", it="Giorni alla settimana:", nl="Dagen per week:"),
     "minutes": dict(en="Minutes per day:", de="Minuten pro Tag:", it="Minuti al giorno:", nl="Minuten per dag")
-    }
+}
 
 mpam = {"enjoyment": dict(en="Because it is fun", de="Weil es Spaß macht", it="Perché è divertente",
                           nl="Omdat het leuk is"),
@@ -46,7 +46,7 @@ mpam = {"enjoyment": dict(en="Because it is fun", de="Weil es Spaß macht", it="
         "false": dict(en="strongly disagree", de="stimme gar nicht zu", it="fortemente in disaccordo",
                       nl="helemaal mee oneens")
         }
-activity_name = {"cycling": dict(en="Cycling", de="Radfahren", it="Ciclismo", nl="Wielersport"),
+activity_name = {"cycling": dict(en="Cycling", de="Radfahren", it="Ciclismo", nl="Fietsen"),
                  "walking": dict(en="Walking", de="Spazieren gehen", it="A passeggio", nl="wandelen"),
                  "shoveling": dict(en="Shoveling", de="Schneeschaufeln", it="Spalare la neve",
                                    nl="Om sneeuw te scheppen"),
@@ -60,7 +60,143 @@ content_title = {"bullet point": dict(en="Tip", de="Tipp", it="Mancia", nl="Tip"
                  "description": dict(en="Description", de="Beschreibung", it="Descrizione", nl="Omschrijving"),
                  "advantage": dict(en="Advantage", de="Vorteil", it="Vantaggio", nl="Voordeel"),
                  "warning": dict(en="Warning", de="Warnung", it="Avvertimento", nl="Waarschuwing"),
-                 "contraindication": dict(en="Warning", de="Kontraindikation", it="Controindicazione", nl="Contra-indicatie")}
+                 "contraindication": dict(en="Warning", de="Kontraindikation", it="Controindicazione",
+                                          nl="Contra-indicatie")}
 
-weekly_credits = dict(en="Credits for this week: ", de="Credits für diese Woche: ", it="Crediti per questa settimana: ", nl="Credits voor deze week: ")
-allocate_credits = dict(en="Unassigned credits: ", de="Nicht vergebene Credits: ", it="Crediti non assegnati: ", nl="Niet-toegewezen kredieten: ")
+greetings = {"morning": dict(en="Good morning", de="Guten Morgen", it="Buongiorno", nl="Fietsen"),
+             "afternoon": dict(en="Good afternoon", de="Guten Tag", it="Buon pomeriggio",
+                                   nl="Goede dag"),
+             "evening": dict(en="Good evening", de="Guten Abend", it="Buona serata", nl="Goedenavond")
+                 }
+title_goal_screen = {"weekly_credits": dict(en="Your Credits for this week: ", de="Ihre Credits für diese Woche: ",
+                                            it="Crediti per questa settimana: ",
+                                            nl="Credits voor deze week: "),
+                     "new_week": dict(en="Welcome to a new week!", de="Willkommen in einer neuen Woche!",
+                                      it="Crediti per questa settimana: ",
+                                      nl="Credits voor deze week: "),
+                     "allocate_credits": dict(en="Unassigned credits: ", de="Nicht vergebene Credits: ",
+                                              it="Crediti non assegnati: ",
+                                              nl="Niet-toegewezen kredieten: "),
+                     "activity_missed": dict(
+                         en="Missed activity: ",
+                         de="Verpasste Aktivität: ",
+                         it="Crediti per questa settimana: ",
+                         nl="Credits voor deze week: "),
+                     "activity_today_single": dict(
+                         en="Your activity for today: ",
+                         de="Ihre heutige Aktivität: ",
+                         nl="Contra-indicatie"),
+                     "activity_today_multiple": dict(
+                         en="Your activities for today: ",
+                         de="Ihre heutigen Aktivitäten: ",
+                         it="Controindicazione",
+                         nl="Contra-indicatie"),
+                     }
+
+text_to_speech = {"activity_today": dict(en="You have scheduled this activity for today. If you have it done already "
+                                            "you can finish it by pressing the 'Activity Done' button.",
+                                         de="Du hast diese Aktivität für heute geplant. Wenn Sie die Aktivität "
+                                            "bereits getan haben, kannst du sie abschließen, indem du auf den Knopf "
+                                            "'Aktivität abschließen' klickst.",
+                                         it="Mancia",
+                                         nl="Tip"),
+                  "delete_info": dict(
+                      en="You can delete all days on which you scheduled the activity without achieving it"
+                         "by clicking on the 'Delete activity' button.",
+                      de="Du kannst alle Tage an denen du die Aktivität eingetragen aber nicht "
+                         "gemacht hast, löschen, indem du auf den Knopf 'Aktivität entfernen' klickst.",
+                      it="Descrizione", nl="Omschrijving"),
+                  "new_week": dict(
+                      en="Hello{}! A new week just started which means you get new credits for the upcoming "
+                         "7 days. Your old activities have been deleted, but you can watch your last weeks "
+                         "performance on the goal information screen.",
+                      de="Hallo{}! Eine neue Woche hat gerade begonnen, was bedeutet, dass du für die "
+                         "kommenden 7 Tage neue Credits erhältst. Deine alten Aktivitäten wurden gelöscht "
+                         "aber kein Sorge, du kannst deine Leistung der letzten Woche auf dem Info "
+                         "Bildschirm nachsehen.",
+                      it="Vantaggio", nl="Voordeel"),
+                  "allocate_mets_zero": dict(
+                      en="Hello{}. I see you haven't allocated any credits for this week. Please do so in order "
+                         "to stay active.",
+                      de="Hallo{}. Wie ich sehe hast du noch keine Credits für diese Woche vergeben. Bitte "
+                         "hole das das nach um weiterhin aktiv zu bleiben.",
+                      it="Avvertimento",
+                      nl="Waarschuwing"),
+                  "allocate_mets": dict(
+                      en="Hello{}.I see you haven't allocated all credits for this week. Please do so in order "
+                         "to meet your wekkly goal and stay active.",
+                      de="Hallo{}. Wie ich sehe hast du noch nicht alle Credits für diese Woche vergeben. Bitte "
+                         "hole das nach um dein wöchentliches Ziel zu erreichen und weiterhin aktiv zu bleiben.",
+                      it="Avvertimento",
+                      nl="Waarschuwing"),
+                  "select_activity": dict(en="Please select a new physical activity for the upcoming week.",
+                                          de="Bitte wähle eine eine neue Aktivität für die kommende Woche aus.",
+                                          it="Controindicazione",
+                                          nl="Contra-indicatie"),
+                  "days": dict(en="Please choose on which day(s) you want to exercise.",
+                               de="Bitte wähle aus an welchen Tagen du die Aktivität machen möchstest.",
+                               it="Controindicazione",
+                               nl="Contra-indicatie"),
+                  "days_edit": dict(en="Please choose which day(s) you want to add or remove.",
+                                    de="Bitte wähle aus welche Tage du hinzufügen oder löschen möchtest.",
+                                    it="Controindicazione",
+                                    nl="Contra-indicatie"),
+                  "duration": dict(
+                      en="Please choose a duration for your activity. You can see how the activity affects "
+                         "your weekly goal on the bottom of the screen.",
+                      de="Bitte wähle eine Dauer für deone Aktivität. Am unteren Bildschirmrand kannst "
+                         "du sehen, wie sich die Aktivität auf dein wöchentliches Ziel auswirkt.",
+                      it="Controindicazione",
+                      nl="Contra-indicatie"),
+                  "goal_info": dict(en="Here are some details about your weekly performance.",
+                                    de="Hier sind Details zu deiner wöchentlichen Leistung.",
+                                    it="Controindicazione",
+                                    nl="Contra-indicatie"),
+                  "activity_today_single": dict(
+                      en="Hey{}, I see you have an open activity for today. Plan your day accordingly so "
+                         "you have enough time for it. When you have done it already you can finish "
+                         "it by pressing the 'Activity Done' button on the activity screen.",
+                      de="Hey{}, ich sehe, du hast heute eine offene Aktivität. Plane deinen Tag entsprechend, "
+                         "damit du genug Zeit dafür hast. Wenn du die Aktivität bereits getan hast, kannst du sie "
+                         "abschließen, indem du auf dem Aktivitätsbildschirm auf den Knopf 'Aktivität abschließen' klickst.",
+                      it="Controindicazione",
+                      nl="Contra-indicatie"),
+                  "activity_today_multiple": dict(
+                      en="Hey{}, I see you have open activities for today. Plan your day accordingly so "
+                         "you have enough time for them. When you have done one of them already you can finish "
+                         "it by pressing the 'Activity Done' button on the activity screen.",
+                      de="Hey{}, ich sehe, du hast heute mehrere offene Aktivitäten. Plane deinen Tag entsprechend, "
+                         "damit du genug Zeit für sie hast. Wenn du eine Aktivität bereits getan hast, kannst du sie "
+                         "abschließen, indem du auf dem Aktivitätsbildschirm auf den Knopf 'Aktivität abschließen' klickst.",
+                      it="Controindicazione",
+                      nl="Contra-indicatie"),
+                  "on_track": dict(
+                      en="Hello there{}. Seems you have a break day today. Still, you can revise your weekly plan"
+                         "or schedule an activity for today if you feel spontanous and fit. Otherwise "
+                         "relax you need to be fit for the upcoming days",
+                      de="Hallo{}. Ich sehe du hast heute einen Pausentag. Trotzdem kannst du deinen Wochenplan überarbeiten "
+                         "oder eine Aktivität für heute planen wenn du spontan bist und dich fit fühlst. Andernfalls "
+                         "entspanne dich, du musst ausgeruht für die kommenden Tage sein.",
+                      it="Crediti per questa settimana: ",
+                      nl="Credits voor deze week: "),
+                  "activity_missed": dict(
+                      en="Good to see you{}! I see you had tasks in the last days which you were not able to do. It is wise"
+                         "to delete them in the activity screen to get credits back which you can use for new activities. That way you make"
+                         "sure you meet your weekly goal.",
+                      de="Schön dich zu sehen{}! Ich sehe, du hattest in den letzten Tagen geplante Aktivitäten, "
+                         "die du nicht durchführen konntest. "
+                         "Kein Problem, lösche diese Aktivitäten einfach im Aktivitätsbildschirm um Credits "
+                         "zurückzugewinnen. Diese können "
+                         "für neue Aktivitäten verwendet werden. Damit stellst du sicher, dass du dein  "
+                         "wöchentliches Ziel erreichst.",
+                      it="Crediti per questa settimana: ",
+                      nl="Credits voor deze week: "),
+                  "activity_info": dict(
+                      en="You can reschedule your days and duration for this activity by clicking the 'Edit Activity' "
+                         "button.",
+                      de="Du kannst die Tage und Dauer dieser Aktivität ändern wenn du auf den Knopf 'Aktivität "
+                         "bearbeiten klickst.",
+                      it="Crediti per questa settimana: ",
+                      nl="Credits voor deze week: "),
+
+                  }
